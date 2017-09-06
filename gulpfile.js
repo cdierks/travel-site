d961433f514e3c3fdae1aa1ec55430629de96da0
cdierks <cdierks@live.de>
2017-09-07 00:11:22 +0200

    Cleand up the gulpfile.js and removed the line that watched for the HTML-function which does not exist in the file.

--- gulpfile.js
+++ gulpfile.js
@@ -12,9 +12,6 @@ gulp.task('styles', function() {
 });
 
 gulp.task('watch', function() {
-  watch('./app/index.html', function() {
-    gulp.start('html');
-  });
   watch('./app/assets/styles/**/*.css', function() {
     gulp.start('styles');
   });
