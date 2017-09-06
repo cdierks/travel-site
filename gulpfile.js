93f1cf842f536ce71d68ef612d9e6f37c5c5bd64
Christian William Dierks <cdierks@me.com>
2017-09-06 23:42:37 +0200

    Added 'watch-task' to the gulp file and generated a style.css that is copied to a temp-folder.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 var gulp = require('gulp'),
 watch = require('gulp-watch');
 
-gulp.task('html', function() {
-  console.log("Imagine something useful being done to your HTML here.")
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
 });
 
-gulp.task('watch', function(){
+gulp.task('watch', function() {
   watch('./app/index.html', function() {
-    gulp.start('html')
-  })
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', function() {
+    gulp.start('styles');
+  });
 });
