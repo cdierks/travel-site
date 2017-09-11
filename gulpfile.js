785cd0bc7958d8d9a727698cc8f576bfdfbc0b2d
Christian William Dierks <cdierks@me.com>
2017-09-12 00:06:12 +0200

    Beautified the file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+  watch = require('gulp-watch'),
+  postcss = require('gulp-postcss'),
+  autoprefixer = require('autoprefixer'),
+  cssvars = require('postcss-simple-vars'),
+  nested = require('postcss-nested'),
+  cssImport = require('postcss-import');
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
